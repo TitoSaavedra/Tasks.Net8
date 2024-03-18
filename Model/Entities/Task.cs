dfab947f46b1122aeef656302eac8f066b913dba
Tito Saavedra Pastén <63429224+TitoSaavedra@users.noreply.github.com>
2024-03-18 16:10:32 -0300

    Inicio
    Se creó el proyecto.
    Se instaló EFCore SqlServer y Tools

---
+++ Model/Entities/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Task.Net8
- Proyecto tareas 
+# Proyecto de tareas en .net 8
+
+## Descripción
+
+Proyecto de tareas en .net 8 con SQL Server y Entity Framework EFCore.
+
+## Tecnologías
+
+- ASP.NET Core 8
+- SQL Server
+- Entity Framework EFCore
+- C#
+- HTML/CSS
+- JavaScript
+- Bootstrap 5
